f54482d3f3c7f365fa7908b3ca23dcacb17d1e70
CKirbs <ckggithub@gmail.com>
2023-10-19 17:16:53 +0800

    Fixed Config Base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/",
+  base: "/vite-deploy-demo/",
   server: {port: 3000},
 });
