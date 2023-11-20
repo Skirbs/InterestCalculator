8be0be3075ce3de57a1fb513ec728fbf378c1718
CKirbs <ckggithub@gmail.com>
2023-11-20 20:14:08 +0800

    Added Comma

--- src/components/ResultsList/ResultsListData.jsx
+++ src/components/ResultsList/ResultsListData.jsx
@@ -2,8 +2,16 @@ const ResultsListData = (props) => {
   return (
     <tr>
       <td>{props.interestData.data[props.dataIndex].year}</td>
-      <td>{props.interestData.data[props.dataIndex].interestAdded}</td>
-      <td>{props.interestData.data[props.dataIndex].futureValue}</td>
+      <td>
+        {props.interestData.data[props.dataIndex].interestAdded
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+      </td>
+      <td>
+        {props.interestData.data[props.dataIndex].futureValue
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+      </td>
     </tr>
   );
 };
